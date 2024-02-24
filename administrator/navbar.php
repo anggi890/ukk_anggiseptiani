c3f17ef0374814fbe352741f066f7bd020949b23
unknown <anggiseptiani541@gmail.com>
2024-02-24 13:18:39 +0700

    first commit

---
+++ administrator/navbar.php
@@ -0,0 +1,10 @@
+<div class="card mt-5">
+            <div class="card-body">
+                <a href="index.php" class="btn btn-info">Beranda</a>
+                <a href="data_barang.php" class="btn btn-info">Data Barang</a>
+                <a href="pembelian.php"  class="btn btn-info">pembelian</a>
+                <!--<a href="stok_barang.php"class="btn btn-success">Stok Barang</a>-->
+                <a href="data_pengguna.php" class="btn btn-info">Data Pengguna</a>
+                <a href="../logout.php" class="btn btn-danger">Keluar</a>
+</div>
+</div>
